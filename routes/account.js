44a74af72a5e8c877e08dfa9ce17515c942c8ce2
jkthomas <tom.januszek@gmail.com>
2019-04-15 20:52:58 +0200

    [ENDPOINT] Unified all endpoints response to be JSON object.

--- routes/account.js
+++ routes/account.js
@@ -5,7 +5,7 @@ const accountQueries = require('../model/DAL/account-dal')
 /* POST signup. */
 router.post('/signup', function (request, response) {
     accountQueries.signup(request)
-        .then((user) => response.status(201).send(`User added with ID: ${user.username}`))
+        .then((user) => response.status(201).send({ 'message': `User added with ID: ${user.username}` }))
         .catch((error) => response.status(400).send({ 'message': error.message }))
 })
 
@@ -14,9 +14,9 @@ router.post('/login', function (request, response) {
     accountQueries.login(request)
         .then((passwordMatch) => {
             if (passwordMatch) {
-                response.status(200).send(`Login completed`)
+                response.status(200).send({ 'message': 'Login completed' })
             } else {
-                response.status(400).send(`Bad password`)
+                response.status(400).send({ 'message': 'Bad password' })
             }
         })
         .catch((error) => response.status(400).send({ 'message': error.message }))
@@ -27,9 +27,9 @@ router.put('/update', function (request, response) {
     accountQueries.updateUserPassword(request)
         .then((passwordMatch) => {
             if (passwordMatch) {
-                response.status(200).send(`User password changed`)
+                response.status(200).send({ 'message': 'User password changed' })
             } else {
-                response.status(400).send(`User password was NOT changed`)
+                response.status(400).send({ 'message': 'User password was NOT changed' })
             }
         })
         .catch((error) => response.status(400).send({ 'message': error.message }))
@@ -40,9 +40,9 @@ router.delete('/delete', function (request, response) {
     accountQueries.deleteUserByUsername(request)
         .then((passwordMatch) => {
             if (passwordMatch) {
-                response.status(200).send(`User deleted`)
+                response.status(200).send({ 'message': 'User deleted' })
             } else {
-                response.status(400).send(`User was NOT deleted`)
+                response.status(400).send({ 'message': 'User was NOT deleted' })
             }
         })
         .catch((error) => response.status(400).send({ 'message': error.message }))
