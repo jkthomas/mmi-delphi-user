8f6da603e9810466e36a1079e7b61ffc4ba924ea
jkthomas <tom.januszek@gmail.com>
2019-05-24 19:45:31 +0200

    [ROUTE] Added account controller functions wrapper

--- routes/account.js
+++ routes/account.js
@@ -4,15 +4,23 @@ const account = require('../controllers/account-controller')
 var accountController = new account.AccountController()
 
 /* POST signup. */
-router.post('/signup', accountController.createAccount)
+router.post('/signup', function (req, res) {
+    accountController.createAccount(req, res)
+})
 
 /* POST login. */
-router.post('/login', accountController.loginAccount)
+router.post('/login', function (req, res) {
+    accountController.loginAccount(req, res)
+})
 
 /* PUT update account password. */
-router.put('/update', accountController.updateAccount)
+router.put('/update', function (req, res) {
+    accountController.updateAccount(req, res)
+})
 
 /* DELETE delete account. */
-router.delete('/delete', accountController.deleteAccount)
+router.delete('/delete', function (req, res) {
+    accountController.deleteAccount(req, res)
+})
 
 module.exports = router
